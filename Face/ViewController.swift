abf4863c4099e09b71a59021709d054d1373e9e6
zahramoh517 <zahramoh517@gmail.com>
2024-03-20 23:10:12 -0400

    Resolved conflict in ViewController.swift

--- Face/ViewController.swift
+++ Face/ViewController.swift
@@ -1,34 +1,39 @@
-//
-//  ViewController.swift
-//  Face
-//
-//  Created by Zahra on 2024-03-17.
-//
-
-import UIKit
+//import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
     
-    // MARK - Variablesssss
+    // MARK: - Variables
+    private let videoDataOutput = AVCaptureVideoDataOutput()
 
     private let captureSession = AVCaptureSession()
+
+    /// Using `lazy` keyword because the `captureSession` needs to be loaded before we can use the preview layer.
     private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
 
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         addCameraInput()
+        showCameraFeed()
+        
+        captureSession.startRunning()
     }
     
-    // MARK: Helper function
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        previewLayer.frame = view.frame
+    }
+    
+    // MARK: - Helper Functions
     
     private func addCameraInput(){
         guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera,.builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else{
-            fatalError("Np camera dectected. Please use a real camera, not a simulator.")
+            fatalError("No camera detected. Please use a real camera, not a simulator.")
         }
         
         let cameraInput = try! AVCaptureDeviceInput(device: device)
@@ -41,5 +46,21 @@ class ViewController: UIViewController {
         previewLayer.frame = view.frame
     }
 
+    private func getCameraFrame(){
+        videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+        
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        
+        captureSession.addOutput(videoDataOutput)
+        
+        guard let connection = videoDataOutput.connections(with: .video), connection.isVideoOrientationSupported else{
+            return
+        }
+        connection.videoOrientation = .portrait
+    }
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate{
+    
+}
