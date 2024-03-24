e737385738628425f53496cf6b783cc4aab95618
zahramoh517 <97250237+zahramoh517@users.noreply.github.com>
2024-03-23 22:48:29 -0400

    Create dataSet.py
    synthetic data set

---
+++ dataSet.py
@@ -0,0 +1,8 @@
+!pip install --upgrade diffusers[torch]
+!pip install transformers
+
+from diffusers import DiffusionPipeline
+import torch
+
+pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipeline.to("cuda")
